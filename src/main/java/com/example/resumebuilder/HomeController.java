7d0c8bf82fded666fc53353d615d3b8c7a445aa2
sudipta-samanta <sudiptaflash@gmail.com>
2021-08-26 00:00:25 +0530

    Added profile view path + Thymleaf

--- src/main/java/com/example/resumebuilder/HomeController.java
+++ src/main/java/com/example/resumebuilder/HomeController.java
@@ -1,9 +1,12 @@
 package com.example.resumebuilder;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -14,5 +17,11 @@ public class HomeController {
     @GetMapping("/edit")
     public String edit(){
         return "edit page";
+    }
+
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model){
+        model.addAttribute("userId", userId);
+        return "profile";
     }
 }

825c00caab17c9743d9d5feef706673edc7d161f
sudipta-samanta <sudiptaflash@gmail.com>
2021-08-29 09:36:13 +0530

    Added basic user profile info and displayed in the template

--- src/main/java/com/example/resumebuilder/models/UserProfile.java
+++ src/main/java/com/example/resumebuilder/models/UserProfile.java
@@ -11,6 +11,11 @@ public class UserProfile {
     private String userName;
     private int theme;
     private String summary;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String designation;
 
     public int getId() {
         return id;
@@ -42,5 +47,45 @@ public class UserProfile {
 
     public void setSummary(String summary) {
         this.summary = summary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into user(id, user_name, password, active, roles) values
-(1, 'foo', 'foo', 'true', 'USER'),
-(2, 'bar', 'bar', 'true', 'USER');
+insert into user (id, user_name, password, active, roles) values
+(1, 'amit', 'foo', 'true', 'USER'),
+(2, 'raj', 'foo', 'true', 'USER');
 
-insert into user_profile (id, user_name, theme, summary) values
-(1, 'foo', 1, 'User name foo'),
-(2, 'bar', 1, 'User name bar');
+insert into user_profile (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
+(1, 'amit', 1, 'Asian cuisine specialist', 'Amit', 'Das', 'amit_das@gmail.com', '1234', 'Chef'),
+(2, 'raj', 1, 'Passonate software developer', 'Raj', 'Vikram', 'raj.vik@gmail.com', '2345', 'SDE');
 
 

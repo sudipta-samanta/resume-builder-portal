bc1db765ce2f3e30660030abfc966b37b80258ff
sudipta-samanta <sudiptaflash@gmail.com>
2021-08-29 12:33:49 +0530

    Added job responsibilites and show them in template

--- src/main/java/com/example/resumebuilder/HomeController.java
+++ src/main/java/com/example/resumebuilder/HomeController.java
@@ -33,6 +33,9 @@ public class HomeController {
         job1.setStartDate(LocalDate.of(2020, 1, 1));
         //job1.setEndDate(LocalDate.of(2020, 3, 1));
         job1.setCurrentJob(true);
+        job1.getResponsibilities().add("Responsible for seasonal menus featuring local seafood");
+        job1.getResponsibilities().add("Performed staff education on cooking techniques necessary for components and assembly during service");
+        job1.getResponsibilities().add("Coordinated with FOH and staff to make everyone aware of possible menu allergies and nightly specials");
 
         Job job2 = new Job();
         job2.setCompany("Company 2");
@@ -40,6 +43,11 @@ public class HomeController {
         job2.setId(2);
         job2.setStartDate(LocalDate.of(2019, 1, 1));
         job2.setEndDate(LocalDate.of(2019, 12, 30));
+        job2.getResponsibilities().add("Maintain an operating COG of 27 percent");
+        job2.getResponsibilities().add("Oversee all hiring for BOH employee");
+        job2.getResponsibilities().add("Perform weekly food inventory");
+
+
         List<Job> jobs = new ArrayList<>();
         jobs.add(job1);
         jobs.add(job2);
